4b79f620c0db7012e23cb6e8ad881cbec4653c61
cajlonbatista <cajlonbatista@acad.ifma.edu.br>
2020-09-15 16:27:36 -0300

    Margim div searh

--- src/pages/Search/styles.js
+++ src/pages/Search/styles.js
@@ -14,6 +14,7 @@ export const Entry = styled.div`
     background: #B2B2B2;
     display: flex;
     justify-content: center;
+    margin-bottom: 20px;
 `;
 export const Exit = styled.div`
     display: grid;
